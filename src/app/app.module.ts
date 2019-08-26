ea840face0b6ca372f0164d1fb858337878d387a
kmanishsao <kmanishsao@gmail.com>
2019-08-26 15:55:32 +0530

    f

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StoreReducers } from '../core/reducers/reducers';
+import { StoreModule } from '@ngrx/store';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    StoreModule.forRoot( StoreReducers)
   ],
   providers: [],
   bootstrap: [AppComponent]
